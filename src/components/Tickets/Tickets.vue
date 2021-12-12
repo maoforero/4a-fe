a13317149e05d7870557ad13ebeb72ab32fe10c6
maoforero <maforeror@gmail.com>
2021-12-12 01:02:56 -0500

    header NavBar

---
+++ src/components/Tickets/Tickets.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div id="container__header">
+            <div id="container__icon">
+                <img src="./assest/game-control.png" alt="logo de pits games">
+                <span id="icon__span">PITS GAMES</span>
+            </div>
+            <nav id="container__menu">
+                <ul id="menu--list">
+                    <li class="list--option"><a href="">Inicio</a></li>
+                    <li class="list--option"><a href="">Juegos</a></li>
+                    <li class="list--option"><a href="">Hardware</a></li>
+                    <li class="list--option"><a href="">Perfil</a></li>
+                    <li class="list--option"><a href="">Lista</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+</template>
