dc4bea5a9cfd4a6a46b9371ed82e6a2a4903db2f
maoforero <maforeror@gmail.com>
2021-12-11 23:50:16 -0500

    login terminado

--- src/components/Session/Login.vue
+++ src/components/Session/Login.vue
@@ -4,20 +4,24 @@
             <div class="container__login--inputData">
                 <div class="login--inputDataUser">
                     <img class="inputDataUserImg"  src="../../assets/user.png"/>
-                    <input type="text">
+                    <input type="text" placeholder="Usuario">
                 </div>
                 <div class="login--inputDataPass">
                     <img class="inputDataPassImg" src="../../assets/key.png"/>
-                    <input type="password">
+                    <input type="password" placeholder="Contraseña">
                 </div>
                 <div class="login--buttons">
                     <div class="login--buttonsSession">
                         <label class="buttonsSession--remember" for=""><input type="checkbox">Recordarme</label>
                         <button class="buttonsSession--login">Iniciar Sesion</button> 
                     </div>
-                    <div class="login--buttonsLinks"></div>
+                    <div class="login--buttonsLinks">
+                        <p><a>Registrarme</a></p>
+                        <p><a>Recuperar contraseña</a></p>
+                    </div>
                 </div>
                 <div class="login--socialMedia">
+                    <p>Iniciar sesión con:</p>
                     <button class="socialMedia--buttonGoogle">Google</button>
                     <button class="socialMedia--buttonTwitter">Twitter</button>
                     <button class="socialMedia--buttonFacebook">Facebook</button>
@@ -44,15 +48,29 @@
 }
 
 .container__login{
-    border: 1px solid black;
+    background-color:#F3F4ED;
+    border: none;
+    border-radius:25px;
     display: flex;
     height: 550px;
+    justify-content: space-between;
     margin: auto;
-    padding: 20px;
-    width: fit-content;
+    width: 840px;
 }
 
 /* imagen principal*/
+
+
+.container__login--mainImage{
+    align-items: center;
+    background-color: rgba(24,179,2,1);
+    border-radius: 25px;
+    display:flex;
+    justify-content:center;
+    width:550px;
+}
+
+
 
 .mainImage{
     height: 430px;
@@ -61,10 +79,12 @@
 
 
 .container__login--inputData{
-    aling-content: center;
+    align-self:center;
     display: flex;
     flex-direction: column;
-    width:auto
+    justify-content:center;
+    margin:0 auto;
+    width:28%;
 }
 
 
@@ -82,8 +102,8 @@
     display:flex;
     height:35px;
     justify-self:center;
-    padding:2px;
-    width:35px
+    padding:6px;
+    width:35px;
 }
 
 .login--inputDataUser > input{
@@ -92,10 +112,12 @@
     border-radius: 2px;
     display: flex;
     flex-direction: column;
+    font-size:1.2rem;
     height: 35px;
     justify-self: center;
     margin: 5px;
-    width: auto
+    padding-left:5px;
+    width: 85%;
 }
 
 .login--inputDataPass > input {
@@ -104,22 +126,78 @@
     border-radius: 2px;
     display: flex;
     flex-direction: column;
+    font-size:1.2rem;
     height: 35px;
     justify-self: center;
     margin: 5px;
-    width: auto
+    padding-left:5px;
+    width: 85%;
 }
 
 .login--buttons{
-
-}
-
-.login--socialMedia{
     display: flex;
     flex-direction: column;
-    height: 100px;
+    margin-top: 20px;
 }
 
+.login--buttonsSession{
+    align-items:center; 
+    display: flex;
+    height:40px;
+    justify-content:space-between;
+    margin-left: 7px;
+    width: 91%;
+}
+
+.buttonsSession--remember > input{
+    align-self: center;
+    margin-right:2px;
+}
+
+.buttonsSession--login{
+    background-color: #2ecc71;
+    border: none;
+    border-radius: 3px;
+    border-bottom: 5px solid #27ae60;
+    color: #FFF;
+    cursor:pointer;
+    float: left;
+    font-size: 12px;
+    height:30px;
+    text-decoration: none;
+    text-shadow: 0px -2px #27ae60;
+    -webkit-transition: all 0.1s;
+    transition: all 0.1s;
+    width:100px;
+}
+
+.buttonsSession--login:hover, .buttonsSession--login:active {
+    border-bottom: 1px solid #2ecc71;
+    -webkit-transform: translate(0px,5px);
+    -ms-transform: translate(0px,5px);
+    transform: translate(0px,5px);
+}
+
+.login--buttonsLinks{
+    display: flex;
+    flex-direction:row;
+    justify-content:space-between;
+    margin:13px 0 0 7px;
+    width:91%
+}
+
+.login--buttonsLinks > p {
+    color: rgba(24,179,2,1);
+    cursor:pointer;
+}
+
+.login--buttonsLinks > p > a{
+    font-size: 12px;
+}
+
+.login--buttonsLinks > p > a:hover{
+    text-decoration: underline;
+}
 
 /* Buttons Social Media*/
 
@@ -127,13 +205,38 @@
     align-items: center;
     display: flex;
     flex-direction: column;
+    margin-top:25px;
     justify-content: center;
 }
 
-.login--socialMedia > button {
-    height: 60px;
-    width: 90%
-
+.login--socialMedia > p {
+    font-size: 12px;
+    margin-bottom:10px;
 }
+
+.login--socialMedia > button {
+    background-color: #2ecc71;
+    border-radius: 3px;
+    border: none;
+    border-bottom: 5px solid #27ae60;
+    color: #FFF;
+    cursor:pointer;
+    font-size: 12px;
+    height: 28px;
+    margin-top:9px;
+    transition: all 0.1s;
+    text-decoration: none;
+    text-shadow: 0px -2px #27ae60;
+    -webkit-transition: all 0.1s;
+    width: 90%
+}
+
+.login--socialMedia > button:hover, .login--socialMedia > button:active {
+    -webkit-transform: translate(0px,5px);
+    -ms-transform: translate(0px,5px);
+    transform: translate(0px,5px);
+    border-bottom: 1px solid #2ecc71;
+}
+
 
 </style>
